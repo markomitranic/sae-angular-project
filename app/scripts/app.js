d9be4c7e1b73c8955d445b4b081f7f6da5ae1f54
Your Name <you@example.com>
2017-02-10 18:19:00 +0100

    finished for today

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,7 +11,11 @@ myApp.config(function ($routeProvider){
 	.when('/home', {
 		redirectTo: '/'
 	})
-	.when('/category/:category_name', {
+	.when('/category/:category_name/', {
+		templateUrl: 'partials/category.html',
+		controller: 'categoryController'
+	})
+	.when('/category/:category_name/:id', {
 		templateUrl: 'partials/category.html',
 		controller: 'categoryController'
 	})
@@ -25,19 +29,48 @@ myApp.config(function ($routeProvider){
 });
 
 
+myApp.controller('headerController', function($scope, $http) {
+	$http.get('partials/categories.json')
+	.success(function(data) {
+		$scope.categories = data;
+	});
+});
 
 
 myApp.controller('homeController', function($scope, $http) {
 
-	$scope.ime = "Hello World";
+	$http.get('partials/categories.json')
+	.success(function(data) {
+		$scope.categories = data;
+	});
 
 });
 
 myApp.controller('categoryController', function($scope, $http, $routeParams) {
-	$scope.ime = "SUPA";
+	$http.get('partials/data.json')
+	.success(function(data) {
+		$scope.dishes = data[$routeParams.category_name];
+
+		if (!$routeParams.id) {
+			window.location.href = '#/category/' + $routeParams.category_name + "/" + $scope.dishes[0].id;
+		} else {
+			$scope.current = $scope.findInDishes($routeParams.id);
+		}
+
+	});
+
+	$scope.findInDishes = function(id) {
+		var marko = null;
+		$scope.dishes.forEach(function(dish, key) {
+			if (dish.id == id) {
+				marko = dish;
+			}
+		});
+		return marko;
+	}
 
 
-	console.log($routeParams.category_name);
+
 
 });
 
